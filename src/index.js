a07bd27c90c2dac716bcc35b4f8052b834b00993
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-04-26 18:39:39 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
