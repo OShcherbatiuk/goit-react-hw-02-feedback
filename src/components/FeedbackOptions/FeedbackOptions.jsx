02e55e58abff7a8da6bfca7edccfd8476ad83332
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-04-27 15:53:56 +0300

    upd

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 
+import s from './FeedbackOptions.module.css';
+
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div>
       {options.map(option => (
         <button
+          className={s.btn}
           key={option}
           type="button"
           value={option}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import Notification from '../Notification';
 
+import s from './Statistics.module.css';
+
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <>
       {total ? (
-        <ul>
+        <ul className={s.list}>
           <li>Good: {good}</li>
           <li>Neutral: {neutral}</li>
           <li>Bad: {bad}</li>
